efdd848998f47d1c5fea4ae926131569844ae5c3
MILENA LONDOÑO <samilolo8778@gmail.com>
2026-04-01 02:24:32 -0500

    Phase 3 - Part 1 New 3 entities created: Referee, Tornament and TournamentTeam *Pending: Part 2: DTOs, AutoMappers and Controllers.

---
+++ SportsLeague.DataAccess/Repositories/TournamentRepository.cs
@@ -0,0 +1,36 @@
+﻿using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+using SportsLeague.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsLeague.DataAccess.Repositories
+{
+    public class TournamentRepository : GenericRepository<Tournament>, ITournamentRepository
+    {
+        public TournamentRepository(LeagueDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Tournament>> GetByStatusAsync(TournamentStatus status)
+        {
+            return await _dbSet
+                .Where(t => t.Status == status)
+                .ToListAsync();
+        }
+
+        public async Task<Tournament?> GetByIdWithTeamsAsync(int id)
+        {
+            return await _dbSet
+                .Where(t => t.Id == id)
+                .Include(t => t.TournamentTeams)
+                    .ThenInclude(tt => tt.Team)
+                    //.ThenInclude(t => t.Players) // Si quieres incluir tambien los jugadores de cada equipo
+                .FirstOrDefaultAsync();
+
+            //Primero estoy en torneo ---> luego para recuperar los equipos de ese torneo, debo pasar 
+            //obligatoriamente por la tabla intermedia TournamentTeams,
+            //y luego deahí recuperar los equipos asociados a ese torneo. Por eso uso el ThenInclude.
+        }
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Repositories/ITournamentTeamRepository.cs
@@ -0,0 +1,10 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ITournamentTeamRepository : IGenericRepository<TournamentTeam>
+    {
+        Task<TournamentTeam?> GetByTournamentAndTeamAsync(int tournamentId, int teamId); // Método para obtener una relación específica entre torneo y equipo
+        Task<IEnumerable<TournamentTeam>> GetByTournamentAsync(int tournamentId); // Método para obtener todas las relaciones de equipos en un torneo específico
+    }
+}
